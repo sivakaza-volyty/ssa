6f531e9997478b9ccb4ed796c31126cfb61ea342
sivakaza-volyty <sivaji@volyty.com>
2016-04-15 17:21:30 +0530

    sonata admin installed and workig, created categories and blogposts

---
+++ src/AppBundle/Admin/BlogPostAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class BlogPostAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('title')
+            ->add('body')
+            ->add('draft')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('title')
+            ->add('body')
+            ->add('draft')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Content', array('class' => 'col-md-6'))
+            ->add('title', 'text')
+            ->add('body', 'textarea')
+            ->end()
+
+            ->with('Meta data' , array('class' => 'col-md-3'))
+            ->add('category', 'sonata_type_model', array(
+                'class' => 'AppBundle\Entity\Category',
+                'property' => 'name',
+            ))
+            ->end()
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('title')
+            ->add('body')
+            ->add('draft')
+        ;
+    }
+}
